53712ad1dfc5d8c44f1b400fc4b15aa30305eea9
dharmendra1104 <dharmendra11042@gmail.com>
2024-02-05 01:23:47 -0800

    reapeat

---
+++ script.js
@@ -0,0 +1,25 @@
+
+window.addEventListener('DOMContentLoaded', function() {
+    const input = document.querySelector('.input');
+    const buttons = document.querySelectorAll('.button');
+ 
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const buttonText = button.textContent;
+ 
+            if (buttonText === 'C') {
+                input.value = '';
+            } else if (buttonText === 'D') {
+                input.value = input.value.slice(0, -1);
+            } else if (buttonText === '=') {
+                try {
+                    input.value = eval(input.value);
+                } catch {
+                    input.value = 'Error';
+                }
+            } else {
+                input.value += buttonText;
+            }
+        });
+    });
+ });
